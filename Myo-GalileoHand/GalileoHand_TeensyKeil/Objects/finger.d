5852c1572116c2a729de926dd8164cb368378fdf
Juan Diego Cardona Mena <15000787@galileo.edu>
2018-08-22 12:32:06 -0600

    MYO - Galileo Hand
    Upload archives related to the Hand's movement

---
+++ Myo-GalileoHand/GalileoHand_TeensyKeil/Objects/finger.d
@@ -0,0 +1,14 @@
+.\objects\finger.o: finger.c
+.\objects\finger.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\finger.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\finger.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\objects\finger.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_K20_DFP\1.5.2\Device\Include\MK20D7.h
+.\objects\finger.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\core_cm4.h
+.\objects\finger.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_compiler.h
+.\objects\finger.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_armcc.h
+.\objects\finger.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_K20_DFP\1.5.2\Device\Include\system_MK20D7.h
+.\objects\finger.o: drivers.h
+.\objects\finger.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\arm_math.h
+.\objects\finger.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\core_cm4.h
+.\objects\finger.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
+.\objects\finger.o: finger.h
---
+++ Myo-GalileoHand/GalileoHand_TeensyKeil/main.c
@@ -0,0 +1,188 @@
+/* Universidad Galileo
+ * Turing Research Lab
+ * Julio E. Fajardo
+ * Galileo Bionic Hand
+ * CMSIS-DSP Application
+ * Embedded Prostheses Controller
+ * May-09-2017
+ * main.c
+ */
+
+#define ARM_MATH_CM4
+
+#include "MK20D7.h"                     					// Device header
+#include "arm_math.h"                   					// ARM::CMSIS:DSP
+#include <stdlib.h>
+#include <stdio.h>
+#include "drivers.h"
+#include "finger.h"
+
+#define DEACTIVATED 0
+#define ACTIVATED   1
+
+int16_t value = 0;
+char debug[32];
+
+int16_t encoding = 0;
+
+typedef struct electrode{
+  q15_t mean;
+  q15_t threshold;
+  q15_t buffer[SIZE];
+  q15_t rectified[SIZE];
+  } electrodes;
+
+electrodes E1 = {0,100};
+electrodes E2 = {0,50};
+
+uint8_t muscle_state = DEACTIVATED;
+
+fingers thumb_rot = {WAITC,6,0,200,0,80};
+fingers thumb_f =   {WAITC,5,0,200,0,80};
+fingers index_f =   {WAITC,4,0,200,0,80};
+fingers middle_f =  {WAITC,3,0,200,0,100};
+fingers ring_f =    {WAITC,2,0,200,0,80};
+fingers little_f =  {WAITC,1,0,200,0,80};
+
+const uint8_t actions[8][6] = { CLOSE, CLOSE, CLOSE, CLOSE, CLOSE, CLOSE,   // Power Grip 
+                                CLOSE, CLOSE, CLOSE, OPEN,  CLOSE, CLOSE,   // Point
+                                OPEN,  OPEN,  OPEN,  CLOSE, CLOSE, CLOSE,   // Pinch
+                                CLOSE, CLOSE, CLOSE, CLOSE, OPEN,  OPEN,    // Hook
+                                CLOSE, CLOSE, CLOSE, CLOSE, CLOSE, OPEN,    // Lateral
+                                CLOSE, CLOSE, OPEN,  OPEN,  CLOSE, CLOSE,   // Peace
+                                OPEN,  OPEN,  OPEN,  OPEN,  OPEN,  OPEN,	  // Rest
+																OPEN,  OPEN,  OPEN,  CLOSE, OPEN,  OPEN			// ~(Point)
+                              };
+
+uint8_t btn = 0;                                                            // Activate / deactivate 
+uint8_t cmd = 0;                                                            // LCD commands
+uint32_t ticks = 0;                                                         // 1 ms ticks
+uint8_t i = 0;
+
+int main(void){
+  Switch_Config();
+  LED_Config(); 
+  ADC0_Config();
+  UART0_Config();
+  Output_Config();
+  SysTick_Config(SystemCoreClock/1000);
+  PIT_Init(1000);
+	
+  arm_fill_q15(0, little_f.buffer, SIZE);
+  arm_fill_q15(0, ring_f.buffer, SIZE);
+  arm_fill_q15(0, middle_f.buffer, SIZE);
+  arm_fill_q15(0, index_f.buffer, SIZE);
+  arm_fill_q15(0, thumb_f.buffer, SIZE);
+	
+  arm_fill_q15(0, E1.buffer, SIZE);
+  arm_fill_q15(0, E2.buffer, SIZE);
+	
+  while(1){
+		/*Finger_Close(1);
+		Finger_Close(2);
+		Finger_Close(3);
+    Finger_Close(4);*/
+    
+		arm_abs_q15(E1.buffer,E1.rectified,SIZE);
+    arm_abs_q15(E2.buffer,E2.rectified,SIZE);
+    arm_mean_q15(E1.rectified,SIZE,&E1.mean);
+    arm_mean_q15(E2.rectified,SIZE,&E2.mean);
+		
+    //switch(muscle_state){
+	  switch(btn){
+      case DEACTIVATED:{
+        if(E1.mean>E1.threshold){
+          LED_On();
+          muscle_state = ACTIVATED;
+        }
+      } break;
+      case ACTIVATED:{
+        if(E2.mean>E2.threshold){
+          LED_Off();
+          muscle_state = DEACTIVATED;
+        }
+      } 
+    }
+		
+    if(btn){ 
+			//switch(cmd){
+			switch(btn){
+        case POWER:    Hand_Action(POWER);    break;
+        case POINT:    Hand_Action(POINT);    break;		
+        case PINCH:    Hand_Action(PINCH);    break;
+        case HOOK:     Hand_Action(HOOK);     break;
+        case LATERAL:  Hand_Action(LATERAL);  break;
+        case PEACE:    Hand_Action(PEACE);    break;
+				case NOTPOINT: Hand_Action(NOTPOINT); break;
+        default:       Hand_Action(POWER); 
+      }
+    }
+    else Hand_Action(REST);
+    //Finger_Close(1);
+    //Finger_Open(1);
+  }
+}
+
+void SysTick_Handler(void) {
+  //LED_On();		
+  little_f.buffer[ticks%SIZE] = (int16_t) ADC0_Read(2);
+  ring_f.buffer[ticks%SIZE]   = (int16_t) ADC0_Read(3);
+  middle_f.buffer[ticks%SIZE] = (int16_t) ADC0_Read(4);
+  index_f.buffer[ticks%SIZE]  = (int16_t) ADC0_Read(5);
+  thumb_f.buffer[ticks%SIZE]  = (int16_t) ADC0_Read(6);
+	
+  Finger_Timing(&little_f);
+  Finger_Timing(&ring_f);
+  Finger_Timing(&middle_f);
+  Finger_Timing(&index_f);
+  Finger_Timing(&thumb_rot);
+	
+  ticks++; 
+  //LED_Off();
+}
+
+//Aquí hay que cambiarlo para adaptarlo para bluetooth
+void PIT0_IRQHandler(void){
+  PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK;																										
+  E1.buffer[ticks%SIZE] = ADC0_Read(0)-2850;
+  E2.buffer[ticks%SIZE] = ADC0_Read(1)-2850;
+  //sprintf(debug,"%d\r",ADC0_Read(0)-2048);
+	//UART0_putString(debug);
+  //LED_Toggle();
+}
+
+void PORTC_IRQHandler(void){
+  if(PORTC->PCR[6]&PORT_PCR_ISF_MASK){
+    if(cmd<5) cmd++;
+    else cmd = 0;
+    UART0_send(cmd+'0');
+    PORTC->PCR[6] |= (PORT_PCR_ISF_MASK);
+  }
+  if(PORTC->PCR[7]&PORT_PCR_ISF_MASK){
+    if(btn<1) {
+      btn++;
+      LED_On();
+    }
+    else {
+      btn = 0;
+      LED_Off();
+    }
+    PORTC->PCR[7] |= (PORT_PCR_ISF_MASK);
+  }
+}
+	
+void UART0_RX_TX_IRQHandler(void){
+  uint8_t data ;
+  (void) UART0->S1;
+  data = UART0->D;
+  UART0->D = data;
+}
+
+void Hand_Action(uint8_t hand_action){
+  Finger_Action(&little_f, actions[hand_action][0]);
+  Finger_Action(&ring_f, actions[hand_action][1]);
+  Finger_Action(&middle_f, actions[hand_action][2]);
+  Finger_Action(&index_f, actions[hand_action][3]);
+  Finger_Action(&thumb_f, actions[hand_action][4]);
+  Finger_ActionTime(&thumb_rot, actions[hand_action][5]);
+}
