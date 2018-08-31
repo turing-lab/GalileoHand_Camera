40a0ad8629779e8beb87af4ffa2566f8cf99dd28
Juan Diego Cardona Mena <15000787@galileo.edu>
2018-08-31 09:57:02 -0600

    Added bicep control
    Changed the triggers to facilitate the control using the bicep, adding versatility to the Galileo Hand

--- Myo-GalileoHand/GalileoHand_TeensyKeil/MYO-GalileoHand/MYO-GalileoHand.lua
+++ Myo-GalileoHand/GalileoHand_TeensyKeil/MYO-GalileoHand/MYO-GalileoHand.lua
@@ -5,7 +5,7 @@ scriptUrl = ""
 function onForegroundWindowChange(app, title)
 	--myo.debug("App on Foreground: " .. app .. " , Its title is: " .. title)
 
-	local stringMatch = string.match(title, "PuTTY") ~= nil
+	--[[local stringMatch = string.match(title, "PuTTY") ~= nil
 
 	if(stringMatch) then
 		myo.setLockingPolicy("none")
@@ -13,23 +13,25 @@ function onForegroundWindowChange(app, title)
 		myo.setLockingPolicy("standard")
 	end
 
-	return stringMatch
+	return stringMatch]]
+	myo.setLockingPolicy("none")
+	return true
 end
 
 function onPoseEdge(pose, edge)
 	if(edge == "on") then
 		pose = forLefties(pose)
-		if(pose == "doubleTap") then
+		if(pose == "fist") then
 			myo.keyboard("a", "press")			--Activate action
 			myo.keyboard("return", "press")
-		elseif(pose == "fingersSpread") then
+		elseif(pose == "doubleTap") then
 			myo.keyboard("d", "press")			--Deactivate action
 			myo.keyboard("return", "press")
 		elseif(pose == "waveIn") then
 			myo.keyboard("p", "press")			--move back to Previous action
 			myo.keyboard("return", "press")
 			doubleVibrate()
-		elseif(pose == "waveOut") then
+		elseif(pose == "fingersSpread" or pose == "waveOut") then
 			myo.keyboard("n", "press")			--move on to Next action
 			myo.keyboard("return", "press")
 			myo.vibrate("short")
@@ -57,3 +59,11 @@ function doubleVibrate()
 	dVCount = 0
 	myo.vibrate("short")
 end
+
+--[[
+	Notes regarding wearing the MYO on the bicep:
+	-to create a fist, contract the muscle once
+	-to imitate waveIn, move the arm to the left
+	-rotating the arm on its axis causes a waveOut-/fingersSpreadlike action (more often is fingerSpread)
+	-contracting the bicep twice in a quick manner emulates doubleTap
+]]
