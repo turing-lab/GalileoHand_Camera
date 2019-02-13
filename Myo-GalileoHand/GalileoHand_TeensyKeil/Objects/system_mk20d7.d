c7e55c9cbf24abd4406972a5dbcfe5ba03fe95e7
Juan Diego Cardona <15000787@galileo.edu>
2019-02-13 12:00:53 -0600

    Removed extra directory

--- Myo-GalileoHand/GalileoHand_TeensyKeil/Objects/system_mk20d7.d
+++
@@ -1,7 +0,0 @@
-.\objects\system_mk20d7.o: RTE\Device\MK20DX256xxx7\system_MK20D7.c
-.\objects\system_mk20d7.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_mk20d7.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_K20_DFP\1.5.2\Device\Include\MK20D7.h
-.\objects\system_mk20d7.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\core_cm4.h
-.\objects\system_mk20d7.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_compiler.h
-.\objects\system_mk20d7.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.0.1\CMSIS\Include\cmsis_armcc.h
-.\objects\system_mk20d7.o: C:\Keil_v5\ARM\PACK\Keil\Kinetis_K20_DFP\1.5.2\Device\Include\system_MK20D7.h
